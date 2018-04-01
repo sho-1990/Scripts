45ae363b6a1be139b201f8409eb7b491b4d32ba2
Sho Sato <shsato.t.t.n@gmail.com>
2018-04-02 02:42:39 +0900

    [clean]indent

--- src/bin/a_drawable.rs
+++ src/bin/a_drawable.rs
@@ -3,26 +3,26 @@ use std::io::Error;
 use std::result::Result;
 
 fn main() {
-   let mut dirs: Vec<&str> = vec![];
-   dirs.push("./drawable");
-   dirs.push("./drawable-ldpi");
-   dirs.push("./drawable-mdpi");
-   dirs.push("./drawable-hdpi");
-   dirs.push("./drawable-xhdpi");
-   dirs.push("./drawable-xxhdpi");
-   dirs.push("./drawable-xxxhdpi");
+    let mut dirs: Vec<&str> = vec![];
+    dirs.push("./drawable");
+    dirs.push("./drawable-ldpi");
+    dirs.push("./drawable-mdpi");
+    dirs.push("./drawable-hdpi");
+    dirs.push("./drawable-xhdpi");
+    dirs.push("./drawable-xxhdpi");
+    dirs.push("./drawable-xxxhdpi");
 
-   for d in dirs {
-      match do_create_dir(d) {
-         Ok(_) => { println!("Create! {}", d) }
-         Err(e) => {
-            eprintln!("{}", e.to_string())
-         }
-      }
-   }
+    for d in dirs {
+       match do_create_dir(d) {
+          Ok(_) => { println!("Create! {}", d) }
+          Err(e) => {
+             eprintln!("{}", e.to_string())
+          }
+       }
+    }
 }
 
 fn do_create_dir(path: &str) -> Result<(), Error> {
-   fs::create_dir(path)?;
-   Ok(())
+    fs::create_dir(path)?;
+    Ok(())
 }
